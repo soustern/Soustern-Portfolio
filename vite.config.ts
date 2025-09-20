493b068cb1a0ceca5e4f8e63150fd1c457fb2bc2
soustern <rafaelantoniassivicechio@gmail.com>
2025-09-19 22:24:25 -0300

    Copy shaders to dist build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'shaders/*', // pasta onde estão seus .glsl
+          dest: 'shaders'       // pasta de destino dentro de dist/
+        }
+      ]
+    })
   ],
   base: '/Soustern-Portfolio/'
 })
