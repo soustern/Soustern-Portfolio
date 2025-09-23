a65eeb08fb63069080dcc9d0ae0bf2d6ba88d8c4
soustern <rafaelantoniassivicechio@gmail.com>
2025-09-23 12:26:26 -0300

    Finish HeroPhantomStrings

--- src/js/HeroPhantomStrings.js
+++ src/js/HeroPhantomStrings.js
@@ -1,5 +1,5 @@
 
-const stack = ["HTML", "CSS", "JavaScript", "TypeScript", "Sass", "Tailwind", "React", "Vue.", "Angular", "Svelte", "Redux", "Webpack", "Jest", "Vite", "Cypress", "REST API", "GraphQL", "React Query", "Git", "Vercel", "npm", "Yarn", "Next", ".NET", "ASP.NET", "Blazor", "C#", "F#", "Entity Framework Core", "Windows Forms", "Xamarin", "Visual Studio", "Visual Studio Code", "Azure", "SQL Server", "IIS", "NuGet", "ML.NET", "Python", "C++", "C"];
+const stack = ["HTML", "CSS", "JavaScript", "TypeScript", "Sass", "Tailwind", "React", "Vue.", "Angular", "Svelte", "Redux", "Webpack", "Jest", "Vite", "Cypress", "REST API", "GraphQL", "React Query", "Git", "Vercel", "npm", "Yarn", "Next", ".NET", "ASP.NET", "Blazor", "C#", "F#", "Entity Framework Core", "Windows Forms", "Xamarin", "Visual Studio", "Visual Studio Code", "Azure", "SQL Server", "IIS", "NuGet", "ML.NET", "Python", "C++", "C", "React Native", "Flutter", "Dart"];
 
 document.addEventListener("DOMContentLoaded", () => {
     const hero = document.querySelector('#hero');
@@ -9,40 +9,66 @@ document.addEventListener("DOMContentLoaded", () => {
         heroCoordinates = hero.getBoundingClientRect();
     });
 
-    document.querySelectorAll(`.stack-paragraph`).forEach(stackParagraph => setCoordinates(heroCoordinates, stackParagraph, 0));
+    document.querySelectorAll(`.stack-paragraph`).forEach(stackParagraph => setCoordinates(heroCoordinates, stackParagraph));
 
 });
 
-function setCoordinates(heroCoordinates, stackParagraph, intervalTime)
+function setCoordinates(heroCoordinates, stackParagraph)
 {
 
+    let currentWord = null;
     const updateText = () => {
-        stackParagraph.innerHTML = ``;
-
-        stackParagraph.style.top = `${randomNumberInRange(0, heroCoordinates.height)}px`;
-        stackParagraph.style.left = `${randomNumberInRange(0, heroCoordinates.width)}px`;
-
-        const word = stack[Math.round(randomNumberInRange(0, stack.length - 1))];
         const staggerTime = 75;
+        let fadeOutDuration = 0;
 
-        [...word].forEach((letter, index) => {
-            const stackLetter = document.createElement(`span`);
-            stackLetter.style.opacity = `0`;
-            stackLetter.style.display = `inline-block`;
-            stackLetter.textContent = letter;
-            stackParagraph.appendChild(stackLetter);
-            animateText(stackLetter, index * staggerTime);
-        });
+        if (currentWord) {
+            const paragraphs = document.querySelectorAll(`.stack-paragraph`);
+            for (const paragraph of paragraphs) {
+                let wordCheck = ``;
+                const spans = paragraph.querySelectorAll(`span`);
+                console.log(paragraph);
+                spans.forEach(span => {
+                    wordCheck += span.textContent;
+                    if (wordCheck === currentWord) {
+                        [...spans].reverse().forEach((span, index) => {
+                            span.classList.remove('animate-glowUpTypewriter');
+                            animateText(span, index * staggerTime, 'animate-glowDownTypewriter');
+                        })
+                    }
+                })
+                fadeOutDuration = spans.length * staggerTime + randomNumberInRange(1000, 3000);
+            }
+        }
+
+        setTimeout(() => {
+            stackParagraph.innerHTML = ``;
+
+            stackParagraph.style.top = `${randomNumberInRange(0, heroCoordinates.height)}px`;
+            stackParagraph.style.left = `${randomNumberInRange(0, heroCoordinates.width)}px`;
+
+            const word = stack[Math.round(randomNumberInRange(0, stack.length - 1))];
+            [...word].forEach((letter, index) => {
+                const stackLetter = document.createElement(`span`);
+                stackLetter.style.opacity = `0`;
+                stackLetter.style.display = `inline-block`;
+                stackLetter.textContent = letter;
+                stackParagraph.appendChild(stackLetter);
+                animateText(stackLetter, index * staggerTime, 'animate-glowUpTypewriter');
+            });
+
+            currentWord = word; 
+        }, fadeOutDuration);
     }
     
-    updateText();
-
-    setInterval(updateText, randomNumberInRange(2000, 8000));
+    updateText(currentWord);
+    setInterval(updateText, randomNumberInRange(4000, 10000));
 };
 
-function animateText(span, duration) {
+function animateText(span, duration, animation) {
     setTimeout(() => {
-        span.classList.add(`animate-glowUpTypewriter`);
+        span.classList.add(animation);
+        span.style.opacity = `.12`;
+        span.style.filter = `drop-shadow(0 0 10px rgba(255,255,255,0.8))`;
     }, duration);
 };
 
